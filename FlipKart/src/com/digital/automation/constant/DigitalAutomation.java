371ffb8d1d28e6fd31640fec3a7c14d28a5292f0
mohitniet10 <mohitniet10@gmail.com>
2017-03-27 23:05:41 +0530

    Added new file

---
+++ FlipKart/src/com/digital/automation/constant/DigitalAutomation.java
@@ -0,0 +1,22 @@
+package com.digital.automation.constant;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author mohit ch
+ *
+ */
+public class DigitalAutomation {
+	
+	@Test
+	public void display() {
+		System.out.println("Testing without main");
+	}
+	
+	@Test
+	public void display1() {
+		System.out.println("Testing.... ");
+	}
+
+}
