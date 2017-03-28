d5be1d6b56c89a36525a8fb4dcbf4d5a53c545e0
mohitniet10 <mohitniet10@gmail.com>
2017-03-28 18:57:35 +0530

    modified file

--- FlipKart/src/com/automation/account/orders/MyOrdersTest.java
+++ FlipKart/src/com/automation/account/orders/MyOrdersTest.java
@@ -1,5 +1,39 @@
 package com.automation.account.orders;
 
-public class MyOrdersTest {
+import java.util.Properties;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.automation.utility.AutomationUtility;
+
+/**
+ * 
+ * @author mohit ch
+ *
+ */
+
+public class MyOrdersTest {
+	@Test
+	public void testProp() throws Exception {
+		Properties obj = AutomationUtility.getProperty();
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get(obj.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.findElement(By.id(obj.getProperty("login"))).click();
+		driver.findElement(By.id(obj.getProperty("mobileno"))).sendKeys(obj.getProperty("username"));
+		driver.findElement(By.id(obj.getProperty("password"))).sendKeys(obj.getProperty("pass"));
+		driver.findElement(By.id(obj.getProperty("submit"))).click();
+		driver.findElement(By.id(obj.getProperty("account"))).click();
+		driver.findElement(By.id(obj.getProperty("myorders"))).click();
+		driver.findElement(By.id(obj.getProperty("vieworders"))).click();
+
+
+		
+	
+
+}
 }
--- FlipKart/src/com/digital/automation/constant/OrdersConstant.java
+++ FlipKart/src/com/digital/automation/constant/OrdersConstant.java
@@ -1,5 +1,14 @@
 package com.digital.automation.constant;
 
-public class OrdersConstant {
 
+/**
+ * 
+ * @author mohit ch
+ *
+ */
+public class OrdersConstant {
+	
+	public static String GekoDriver = "webdriver.gecko.driver";
+
+	public static String GekoDriverpath ="C:\\Program Files\\geckodriver-v0.15.0-win64\\geckodriver.exe";
 }
