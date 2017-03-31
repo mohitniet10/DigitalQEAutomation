4af266a094ca4b6816dddb79e1fe67cecf8ac0cf
mohitniet10 <mohitniet10@gmail.com>
2017-03-31 12:22:37 +0530

    modified

--- FlipKart/src/Cookies.java
+++ FlipKart/src/Cookies.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Cookies {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\geckodriver-v0.15.0-win64\\geckodriver.exe");
 WebDriver driver=new FirefoxDriver();
 driver.get("https://www.facebook.com/");
 driver.manage().window().maximize();
