b1e7afbc3f8da0e20f9b3c1b98ae0b893635aa7b
mohitniet10 <mohitniet10@gmail.com>
2017-04-07 10:18:22 +0530

    testng

--- FlipKart/src/com/automation/account/orders/RetryListner.java
+++ FlipKart/src/com/automation/account/orders/RetryListner.java
@@ -1,5 +1,22 @@
 package com.automation.account.orders;
 
-public class RetryListner {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.annotations.ITestAnnotation;
+
+public class RetryListner implements IAnnotationTransformer {
+
+	@Override
+	public void transform(ITestAnnotation testannotation, Class testClass,
+			Constructor testConstructor, Method testMethod)	{
+		IRetryAnalyzer retry = testannotation.getRetryAnalyzer();
+
+		if (retry == null)	{
+			testannotation.setRetryAnalyzer(Retry.class);
+		}
+
+	}
 }
